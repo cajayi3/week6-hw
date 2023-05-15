87c68a9ec69fc3c8d3de70e1fa4b0874c4282106
Christopher Ajayi <christopher@codingtemple.com>
2023-05-14 20:00:34 -0500

    first commit

--- app/blueprint/marine/routes.py
+++ app/blueprint/marine/routes.py
@@ -1,31 +1,23 @@
 from flask import render_template, flash, redirect, url_for
 
 from app.structure import User
+from app import db 
 
 from . import bp
-from app.sculpture import SealsixStructure, MajorSculpture
+from app.sculpture import SealsixStructure
 
 @bp.route('/major', methods=['GET',"POST"])
-def signin():
-    sculpture = MajorSculpture()
-    if sculpture.validate_on_submit():
-        user = User.query.filter_by(username=sculpture.username.data).first()
-        if user and user.check_password(sculpture.password.data):
-            print(f'{sculpture.username.data} signing in')
-        else:
-            print('user doesn\'t exist or incorrect password')
-
-    return render_template('Major.jinja',  sculpture=sculpture)
+def major():
+    return render_template('Major.jinja')
 
 @bp.route('/sealsix', methods=['GET','POST'])
-def register():
+def sealsix():
     sculpture = SealsixStructure()
     if sculpture.validate_on_submit():
         user = User.query.filter_by(username=sculpture.username.data).first()
         email = User.query.filter_by(email=sculpture.email.data).first()
         if not email and not user:
             u = User(username=sculpture.username.data,email=sculpture.email.data)
-            u.password = u.hash_password(sculpture.password.data)
             u.commit()
             flash(f"{sculpture.username.data} registered")
             return redirect(url_for("ranger.home"))
